d512f126ae01f2e69f29fa4c820833b144877856
Emilly Waiandt <137171497+3mil1y@users.noreply.github.com>
2025-04-15 11:13:38 -0300

    fix namespace error

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ require_once "../app/core/autoloader.php"; // Autoload das classes
 
 use App\Core\Router;
 use App\Models\TaskModel;
-use App\Core\Session;
+use App\core\Session;
 
 Session::start(); // Inicia a sessão
 Session::set('tasks', TaskModel::getAll()); // Carrega as tarefas na sessão
